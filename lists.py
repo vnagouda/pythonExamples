66bce63d656c2bb5ce0e474bc805c088094f00fa
Viresh Nagouda <64742359+vnagouda@users.noreply.github.com>
2021-08-01 22:34:52 +0530

    first commit

---
+++ lists.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  1 21:54:30 2021
+
+@author: vnago
+"""
+
+names = ['sainaba', 'viresh', 'navin', 'sudha', 'vivaan', 'ishaan']
+print(names)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(numbers[1:3])
+del numbers[1]
+print(numbers)
+print(names.index('viresh'))
